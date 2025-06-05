be59a1f5e77d221b0adb25f078a3a4d3d07c57a9
John Doe <johndoe@email.com>
2025-06-04 21:28:01 -0500

    more commits

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -26,7 +26,7 @@ export default function Layout(){
           
             <div className={styles.menu}>
              <button className={styles.button} onClick={handleDrop}>
-            <img src={image2} alt="" height={80} width={80}/>
+            <img src={image2} alt="" height={70} width={70}/>
             </button>
             <div className={`${styles.dropdown} ${drop ? styles.dropdown: styles.dropdownhidden}`}>
                 <Link className={styles.Link} to={'/CreateAppointment'}>Book Appointment</Link>
